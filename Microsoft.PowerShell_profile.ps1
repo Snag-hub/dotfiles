36ae2f121cb1e32c16778fedf9989b5fc730f65a
Syed Nadeem Hussain <syednadeem.dev@gmail.com>
2025-10-07 17:43:46 +0530

    Update Microsoft.PowerShell_profile.ps1
    New config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,7 @@
-
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\easy-term.omp.json" | Invoke-Expression
-
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\sonicboom_dark.omp.json" | Invoke-Expression
 # PSReadLine with Vim Mode
 Import-Module PSReadLine
-Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineOption -ViModeIndicator Prompt
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle ListView
 
 # Additional Modules
 Import-Module Terminal-Icons
@@ -16,3 +11,5 @@ Import-Module posh-git
 Set-Alias ll ls
 Set-Alias vim nvim
 Set-Alias g git
+
+oh-my-posh init pwsh | Invoke-Expression
